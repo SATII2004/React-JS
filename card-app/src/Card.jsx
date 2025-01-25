cc75fb7194075a24ec34eda76d323fc6b0c27ae2
N SATISH <162682521+SATII2004@users.noreply.github.com>
2025-01-25 23:45:34 +0530

    .

--- card-app/src/Card.jsx
+++ card-app/src/Card.jsx
@@ -1,13 +1,16 @@
-
+import profilepic from './assets/my-image.jpg'
 
 function Card()
 {
     return(
+        
         <div className="card">
-            <img src="" alt="profile-picture"></img>
-            <h2>SATISH</h2>
-            <p>I am interested in History</p>
+            <img className="card-image" src={profilepic} alt="profile picture" style={{width: "200px", height: "200px"}}/>
+
+            <h2 className="card-title">SATISH</h2>
+            <p className="card-text">I am interested in History</p>
         </div>
+        
     );
 }
 
