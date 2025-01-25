35bd9d241a3775f604d9dac61c31036d86af5a1a
N SATISH <162682521+SATII2004@users.noreply.github.com>
2025-01-25 23:04:49 +0530

    .

---
+++ card-app/src/Card.jsx
@@ -0,0 +1,14 @@
+
+
+function Card()
+{
+    return(
+        <div className="card">
+            <img src="" alt="profile-picture"></img>
+            <h2>SATISH</h2>
+            <p>I am interested in History</p>
+        </div>
+    );
+}
+
+export default Card
