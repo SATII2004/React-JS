f94197f71894d60d678f9e97e91553e006997a57
N SATISH <162682521+SATII2004@users.noreply.github.com>
2025-02-04 21:03:19 +0530

    .

--- my-new-app/src/Button.jsx
+++ my-new-app/src/Button.jsx
@@ -1,6 +1,18 @@
+
+
 function Button()
 {
-    return(<button className="button">Click Me</button>);
+
+
+    const styles = {
+        backgroundColor: "hsl(200,100%,50%)",
+        color: "white",
+        padding: "10px 20px",
+        borderRadius: "5px",
+        border: "none",
+        cursor: "pointer",
+    }
+    return(<button style={styles}>Click Me</button>);
 
 }
 
