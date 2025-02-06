7dce5e1c36ceec36c81a95672a2919fc6b19c990
N SATISH <162682521+SATII2004@users.noreply.github.com>
2025-02-06 18:22:10 +0530

    .

---
+++ props-app/src/App.jsx
@@ -0,0 +1,21 @@
+
+import Student from './Student.jsx'
+
+function App() {
+
+  return (
+    <>
+      <Student name="Spongebob"  age={30} isStudent={true} />
+      <Student name="Maria"  age={42} isStudent={false} />
+      <Student name="Harry"  age={39} isStudent={true} />
+      <Student name="Senorita"  age={26} isStudent={false} />
+      <Student name="Larry" />
+
+      
+    </>
+  )
+}
+
+
+
+export default App
